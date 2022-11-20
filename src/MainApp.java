228a518da319b10b9267b65a52c45adf828b7d0b
MainApp <https://github.com/malik0117/projet_java_studi.git>
2022-11-20 17:27:52 +0100

    Initial commit

---
+++ src/MainApp.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class MainApp
+{
+    public static void main(String[] args)
+    {
+        System.out.println(System.getenv());
+        Scanner sc=new Scanner(System.in);
+
+    }
+}
